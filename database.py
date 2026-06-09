e4f809204e91f336999d3d30fc379ec725c76a08
bmouale03 <bmouale@mail.ru>
2026-06-09 21:58:03 +0000

    Fix Streamlit Secrets

--- database.py
+++ database.py
@@ -1,18 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import streamlit as st
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://admin:admin123@postgres:5432/smartfamily"
-)
+DATABASE_URL = None
 
-print("DATABASE_URL =", DATABASE_URL)
+# Streamlit Cloud Secrets
+if "DATABASE_URL" in st.secrets:
+    DATABASE_URL = st.secrets["DATABASE_URL"]
+
+# Fallback local
+if DATABASE_URL is None:
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL",
+        "postgresql://admin:admin123@postgres:5432/smartfamily"
+    )
 
 engine = create_engine(
     DATABASE_URL,
-    echo=False
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
