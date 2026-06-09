869894a7c5e16e7429b853a6987c895fec4d90af
bmouale03 <bmouale@mail.ru>
2026-06-09 21:37:17 +0000

    version deployer avec BD

--- database.py
+++ database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = (
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
     "postgresql://admin:admin123@postgres:5432/smartfamily"
 )
 
+print("DATABASE_URL =", DATABASE_URL)
+
 engine = create_engine(
     DATABASE_URL,
-    echo=True
+    echo=False
 )
 
 SessionLocal = sessionmaker(
