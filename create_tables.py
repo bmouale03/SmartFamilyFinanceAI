66bcd65871f785342e33bc233035d9843a77182c
bmouale03 <bmouale@mail.ru>
2026-06-11 21:49:02 +0000

    Version Streamlit Cloud

--- create_tables.py
+++ create_tables.py
@@ -1,19 +1,10 @@
-from database import Base
-from database import engine
+from database import Base, engine
 
+# import des modèles
 from models.user import User
-from models.membre import Membre
-from models.revenu import Revenu
-from models.depense import Depense
 from models.compte import Compte
-from models.epargne import Epargne
-from models.objectif import Objectif
-from models.devise import Devise
-from models.prediction import Prediction
-from models.utilisateur import *
+from models.depense import Depense
 
 Base.metadata.create_all(bind=engine)
 
-print(
-    "Base SmartFamily créée avec succès."
-)
+print("Base créée")
--- database.py
+++ database.py
@@ -3,11 +3,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-print("===== NOUVELLE VERSION DATABASE.PY =====")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
