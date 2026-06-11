9834d4d66265c93004de1cf0c878b366e976aa0e
bmouale03 <bmouale@mail.ru>
2026-06-11 18:58:45 +0000

    Auto create tables

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from database import Base, engine
-Base.metadata.create_all(bind=engine)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
